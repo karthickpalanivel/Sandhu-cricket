3f20e891cdcb243543ccf50ff39999bcd02a51f8
karthickpalanivel <karthickpalanivel4@gmail.com>
2025-12-14 12:03:09 +0530

    first commit

---
+++ src/screens/SetupScreen.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Setup Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
+
+export default SplashScreen;
